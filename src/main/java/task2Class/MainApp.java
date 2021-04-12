2989e8790cd2af6794c1cf324c5e0bf80debf22b
lev <lev-seredinskiy@mail.ru>
2021-04-12 10:47:11 +0300

    Дз классы

---
+++ src/main/java/task2Class/MainApp.java
@@ -0,0 +1,22 @@
+package task2Class;
+
+import java.util.HashMap;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Person a = new Person(false, "Витя");
+        Person b = new Person(true, "Кристина");
+        Person c = new Person(false, "Виталя");
+        Person d = new Person(true, "Оля");
+
+        a.marry(b);
+        c.marry(d);
+        a.marry(d);
+        a.print();
+        b.print();
+        c.print();
+        d.print();
+
+
+    }
+}
---
+++ src/main/java/task2Class/Person.java
@@ -0,0 +1,48 @@
+package task2Class;
+
+public class Person {
+    private final boolean man;
+    private final String name;
+    private Person spouse;
+
+    public Person(boolean man, String name) {
+        this.man = man;
+        this.name = name;
+    }
+
+    public boolean marry(Person person) {
+        if (this.man != person.man && this.spouse != person) {
+            if (this.spouse != null) {
+                this.divorce();
+            }
+            if (person.spouse != null) {
+                person.divorce();
+            }
+            this.spouse = person;
+            person.spouse = this;
+            return true;
+
+        } else return false;
+    }
+
+    public boolean divorce() {
+        if (this.spouse != null) {
+            this.spouse.spouse = null;
+            this.spouse = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void print() {
+        System.out.println("Имя - " + this.name);
+        if (this.man) {
+            System.out.println("Пол - женский");
+        } else System.out.println("Пол - мужской");
+        if (this.spouse != null){
+            System.out.println("В браке с " + this.spouse.name);
+        } else System.out.println("Не состоит в браке");
+    }
+
+}
